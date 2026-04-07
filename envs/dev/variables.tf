68c16b847862ffbddb51f2cfbe5dcbaed74287f5
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-07 21:02:06 +0530

    Create variables.tf

---
+++ envs/dev/variables.tf
@@ -0,0 +1,11 @@
+variable "db_password" {
+  description = "Master password for the RDS PostgreSQL database"
+  type        = string
+  sensitive   = true
+}
+
+variable "jwt_secret" {
+  description = "JWT signing secret for the application"
+  type        = string
+  sensitive   = true
+}
