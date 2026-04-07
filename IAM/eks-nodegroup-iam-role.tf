e34e1bc2ad327d8b4298ad1ae77ac706329d4954
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-07 14:08:34 +0530

    Update eks-nodegroup-iam-role.tf

--- IAM/eks-nodegroup-iam-role.tf
+++ IAM/eks-nodegroup-iam-role.tf
@@ -21,22 +21,22 @@ resource "aws_iam_role" "eks_nodegroup_role" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
   role       = aws_iam_role.eks_nodegroup_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
   role       = aws_iam_role.eks_nodegroup_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "eks_ecr_policy" {
+resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
   role       = aws_iam_role.eks_nodegroup_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "aws_iam_role_policy_attachment" "eks-AmazonEBSCSIDriverPolicy" {
+resource "aws_iam_role_policy_attachment" "node_AmazonEBSCSIDriverPolicy" {
   role       = aws_iam_role.eks_nodegroup_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
