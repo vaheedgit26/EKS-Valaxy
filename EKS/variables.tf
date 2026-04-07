2b0415b8e7ce2ad2be9b123974a56ac95430e774
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-07 13:48:26 +0530

    Update variables.tf

--- EKS/variables.tf
+++ EKS/variables.tf
@@ -15,3 +15,5 @@ variable eks_cluster_security_group_ids { type = list(string) }
 variable node_instance_types { type = list(string) }
 
 variable node_capacity_type { type = string }
+
+variable node_disk_size { type = number }
