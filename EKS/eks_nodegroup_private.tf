5a2d3241efdf0bb3e1ddbad46d20dca405be08e2
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-07 19:37:33 +0530

    Update eks_nodegroup_private.tf

--- EKS/eks_nodegroup_private.tf
+++ EKS/eks_nodegroup_private.tf
@@ -1,11 +1,11 @@
 # EKS Node Group
 resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
-  node_group_name = "${var.project}-${var.env}-node-group"
-  node_role_arn   = aws_iam_role.node_group.arn
+  node_group_name = "${local.resource_name}-node-group"
+  node_role_arn   = aws_iam_role.eks_nodegroup_role.arn
   subnet_ids      = var.node_subnet_ids
   instance_types  = var.node_instance_types
-  capacity_type   = var.node_capacity_type
+  capacity_type   = var.node_capacity_type   ON_DEMAND/ SPOT
   disk_size       = var.node_disk_size
 
   scaling_config {
@@ -25,7 +25,6 @@ resource "aws_eks_node_group" "main" {
   # Apply labels to each EC2 instance for easier scheduling and management in Kubernetes
   labels = {
     "env"  = var.environment_name
-    "team" = var.business_division
   }
 
   depends_on = [
