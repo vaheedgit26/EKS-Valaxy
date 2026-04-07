f09b4d01f93e4aa03018cfa5431cde30432c1dfd
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-07 19:57:39 +0530

    Update eks-cluster.tf

--- EKS/eks-cluster.tf
+++ EKS/eks-cluster.tf
@@ -10,9 +10,9 @@ resource "aws_eks_cluster" "main" {
   # VPC configuration for control plane networking
   vpc_config {
     subnet_ids              = var.subnet_ids
-    endpoint_private_access = var.endpoint_private_access
-    endpoint_public_access  = var.endpoint_public_access
-    security_group_ids      = var.eks_cluster_security_group_ids
+    endpoint_private_access = var.cluster_endpoint_private_access
+    endpoint_public_access  = var.cluster_endpoint_public_access
+    security_group_ids      = var.cluster_security_group_ids
   }
 
   access_config {
