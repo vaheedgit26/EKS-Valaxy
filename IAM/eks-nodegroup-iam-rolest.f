b612bdcdca2448296cc10ac595fd932c18a7aa4f
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-07 09:55:02 +0530

    Update and rename eks-nodegroup-iam-role.tf to eks-nodegroup-iam-rolest.f

---
+++ IAM/eks-nodegroup-iam-rolest.f
@@ -0,0 +1,38 @@
+resource "aws_iam_role" "eks_nodegroup_role" {
+  # IAM role name following environment and division-based naming
+  name = "${local.name}-eks-nodegroup-role"
+
+  # Trust policy: allow EC2 service to assume this role
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+
+  tags = {
+    Name      = "${var.project}-${var.env}-eks-node-group-role"
+    Env       = var.env
+    Project   = var.project
+    Terraform = true
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.eks_nodegroup_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_nodegroup_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_ecr_policy" {
+  role       = aws_iam_role.eks_nodegroup_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
