6eb872a12a0b5d23c9a5da27f5b645493794c7ea
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-07 13:51:29 +0530

    Update variables.tf

--- EKS/variables.tf
+++ EKS/variables.tf
@@ -16,7 +16,14 @@ variable node_instance_types { type = list(string) }
 
 variable node_capacity_type { type = string }
 
-variable node_disk_size { 
-  type = number 
-  default = 8
+variable "node_disk_size" {
+  type        = number
+  description = "Disk size (in GB) for EKS worker nodes"
+  default     = 8
+
+  validation {
+    condition     = var.node_disk_size >= 10 && var.node_disk_size <= 1000
+    error_message = "Disk size must be between 8GB and 25GB."
+  }
 }
+
