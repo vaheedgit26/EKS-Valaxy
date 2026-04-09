d84eb0ad58859af993371279f2197633c8b81f8a
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-09 10:01:17 +0530

    Update variables.tf

--- terraform/envs/dev/02-nat-instance/variables.tf
+++ terraform/envs/dev/02-nat-instance/variables.tf
@@ -2,6 +2,13 @@ variable "ami_id" {}
 variable "public_key_name" {}
 variable "remote_ip_to_connect_nat_instance" {}
 
+variable "vpc_id" {}
+variable "vpc_cidr" {}
+variable "public_subnet_cidr" {}
+variable "private_subnet_cidr" {}
+variable "public_subnet_ids" { type = list }
+# variable "private_subnet_ids" { type = list }
+
 #############  Tags ###############################################
 variable "project_name" { default = "expense" }
 variable "env" { default = "dev" }
