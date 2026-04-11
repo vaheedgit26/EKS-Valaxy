d88df2f8f3e617a01cd31dfed3d259f724372dff
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-11 22:42:09 +0530

    Create 02-bastion.tf

---
+++ terraform/envs/dev/infra-part/02-bastion.tf
@@ -0,0 +1,19 @@
+# Security Group for Bastion Host
+module "bastion_sg" {
+  source = "git::https://github.com/vaheedgit26/Infra.git//modules/sg"
+  project_name = var.project_name
+  env = var.env
+  vpc_id = var.vpc_id
+  sg_name = "bastion_sg"
+  sg_description = "Bastion Instance Security Group"
+  common_tags = var.common_tags
+}
+
+resource "aws_security_group_rule" "bastion_internet" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.bastion_sg.sg_id    # aws_security_group.sg_nat_instance.id
+}
