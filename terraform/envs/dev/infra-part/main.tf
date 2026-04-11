c49f6028815b2550deee439f737988189d6a2431
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-11 19:30:42 +0530

    Update main.tf

--- terraform/envs/dev/infra-part/main.tf
+++ terraform/envs/dev/infra-part/main.tf
@@ -38,7 +38,7 @@ module "nat_instance" {
 
   remote_ip_to_connect_nat_instance       = "0.0.0.0/0"       # "${var.remote_ip_to_connect_nat_instance}/32"
 
-  is_nat_instance = true
+  # is_nat_instance = true
   is_eip_required = false
 
   project_name = var.project_name
