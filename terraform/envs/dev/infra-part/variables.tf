a82eb595b8e981b161ba8b0cd804351f19e53d64
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-11 20:26:51 +0530

    Update variables.tf

--- terraform/envs/dev/infra-part/variables.tf
+++ terraform/envs/dev/infra-part/variables.tf
@@ -1,3 +1,7 @@
 variable "region" {}          #  Actually for vpc creation, it takes region from provider block (For privider.tf)
 variable "project_name" {}
 variable "env" {}
+
+variable "ami_id" { default = "ami-0ddfba243cbee3768" }
+variable "public_key_name" { default = "mumbai-1" }
+
