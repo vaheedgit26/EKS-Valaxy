0e9ff0423b2c20466f9384fd5f1e5fe568144207
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-11 09:43:30 +0530

    Update main.tf

--- terraform/envs/dev/02-nat-instance/main.tf
+++ terraform/envs/dev/02-nat-instance/main.tf
@@ -8,7 +8,7 @@ module "nat_instance" {
   ami_id                                  = "ami-0ddfba243cbee3768" 
   public_key_name                         = "mumbai-1"
   public_subnet_ID_to_launch_nat_instance = var.public_subnet_ids[0]
-  public_subnet_cidr                      = var.public_subnet_cidr  # for private instance sg purpose
+  public_subnet_cidr                      = var.public_subnet_cidr   # for private instance sg purpose
   private_subnet_cidr                     = var..private_subnet_cidr # for database instance sg purpose
   # private_subnet_ids                      = local.private_subnet_ids #module.vpc.private_subnet_ids 
   remote_ip_to_connect_nat_instance       = "0.0.0.0/0"       # "${var.remote_ip_to_connect_nat_instance}/32"
