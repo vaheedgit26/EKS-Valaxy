d1a8cfe6acd952c4f539f4351f9c207a1e40d582
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-12 18:01:59 +0530

    Delete terraform/envs/dev/05-eks-create/backend.tf

--- terraform/envs/dev/05-eks-create/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "pharma-tf-state"
-    key            = "envs/dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "pharma-tf-lock"
-    encrypt        = true
-  }
-}
