a08ffccc6790d4c5f7d36b37e658c40ff582ebc8
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-13 13:08:46 +0530

    Update variables.tf

--- terraform/envs/dev/05-eks-create/variables.tf
+++ terraform/envs/dev/05-eks-create/variables.tf
@@ -2,6 +2,10 @@ variable "region" {}     # This value can be taken from provider.tf
 variable "project" {}
 variable "env" {}
 
+#############################################################################################################
+variable "s3_bucket_id" {}          # For reading the remote vpc state      (gets value from 'bash script')
+variable "vpc_remote_state_key" {}  # Key for refering the vpc remote state (gets value from 'bash script')
+#############################################################################################################
 
 variable "db_password" {
   description = "Master password for the RDS PostgreSQL database"
