1e44d2282c4f9f9c30786d705649b1772166dfb1
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-13 13:28:12 +0530

    Update main.tf

--- terraform/envs/dev/05-eks-create/main.tf
+++ terraform/envs/dev/05-eks-create/main.tf
@@ -6,16 +6,16 @@ module "eks" {
   project             = var.project  # "pharma"
   env                 = var.env      # "dev"
 
-  cluster_version     = "1.33"
-  cluster_subnet_ids  = module.vpc.private_subnet_ids
+  cluster_version                 = "1.33"
+  cluster_subnet_ids              = [data.terraform_remote_state.vpc.private_subnet_ids]
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = false
-  eks_cluster_security_group_ids  = [module.bastion_sg.sg_id]  # This is additional cluster SG and the default cluster SG is intact
+  eks_cluster_security_group_ids  = [data.terraform_remote_state.vpc.bastion_host_sg_id]  # This is additional cluster SG and the default cluster SG is intact
 
-  node_subnet_ids     = module.vpc.private_subnet_ids
+  node_subnet_ids     = data.terraform_remote_state.vpc.private_subnet_ids
   node_instance_types = ["t3.small"]
   node_capacity_type  = "SPOT"
-  node_addl_sg_ids    = [module.bastion_sg.sg_id]
+  node_addl_sg_ids    = [data.terraform_remote_state.vpc.bastion_host_sg_id]              # This is additional cluster SG and the default cluster SG is intact
   node_ssh_public_key = "us-east-1"
 
   desired_capacity    = 3
@@ -28,10 +28,10 @@ module "rds" {
 
   project               = var.project  # "pharma"
   env                   = var.env      # "dev"
-  subnet_ids            = module.vpc.database_subnet_ids
-  vpc_id                = module.vpc.vpc_id
+  subnet_ids            = [data.terraform_remote_state.vpc.database_subnet_ids]
+  vpc_id                = data.terraform_remote_state.vpc.vpc_id
   # eks_security_group_id = module.eks.cluster_security_group_id
-  rds_allowed_security_group_ids = [module.eks.cluster_security_group_id, module.bastion_sg.sg_id]
+  rds_allowed_security_group_ids = [module.eks.cluster_security_group_id, data.terraform_remote_state.vpc.bastion_host_sg_id]
   db_name               = "pharmadb"
   db_username           = "pharmaadmin"
   db_password           = var.db_password
